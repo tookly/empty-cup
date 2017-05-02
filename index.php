39c65b421e144977769c1a18c4ce849b8dbe3d5e
chengxiaoli <chengxiaoli@qmtv.com>
2017-05-02 20:31:08 +0800

    Squashed 'public/lib/' content from commit 654690a
    git-subtree-dir: public/lib
    git-subtree-split: 654690aeda23b6dc0a3776ea1a931e99e48532a3

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//test git reset
+echo phpinfo();
+
+echo "create a new branch";
+
+echo "another conflicts";
+
+echo "conflits";
+
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+echo "fix bug ";
+
+echo "bug fixed"
+=======
+echo "sth wrong";
+>>>>>>> Stashed changes
+=======
+echo "sth wrong";
+>>>>>>> Stashed changes
